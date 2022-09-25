a958d48a44702d24483d0cbe25082ac8463f081b
mi-xu <mxu11290@gmail.com>
2022-09-25 16:42:09 -0400

    implement create-chambresses with fp-ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import { NextPage } from "next"
-import { useState } from "react"
-import { trpc } from "../utils/trpc"
-import React from "react"
-import { Congress } from "@prisma/client"
+import { NextPage } from 'next'
+import { useState } from 'react'
+import { trpc } from '../utils/trpc'
+import React from 'react'
+import { Chambress, Congress } from '@prisma/client'
 
 const Home: NextPage = () => {
-  const chambresses = trpc.useQuery(["get-chambresses"])
-  const mutation = trpc.useMutation(["create-chambresses"], {
+  const chambresses = trpc.useQuery(['get-chambresses'])
+  const mutation = trpc.useMutation(['create-chambresses'], {
     onSuccess(data, variables, context) {
       if (data == null) {
         return
@@ -22,20 +22,21 @@ const Home: NextPage = () => {
         <h1>Admin</h1>
       </div>
       <div className="p-2 flex flex-col gap-2">
-          <button
-            className="bg-neutral-600 rounded-md p-2"
-            disabled={mutation.isLoading}
-            onClick={() => { mutation.mutate() }}
-          >
-            Create Chambresses
-          </button>
-        </div>
-        <div className="w-full place-content-center p-2 grid grid-cols-[repeat(auto-fill,200px)] grid-rows-[repeat(auto-fill,100px)] gap-2">
-          {chambresses.isLoading && <div>Loading chambresses...</div>}
-          {(chambresses.data ?? []).map((c) => (
-            <ChambressTile key={c.id} chambress={c} />
-          ))}
-        </div>
+        <button
+          className="bg-neutral-600 rounded-md p-2"
+          disabled={mutation.isLoading}
+          onClick={() => {
+            mutation.mutate()
+          }}
+        >
+          Populate Chambresses
+        </button>
+      </div>
+      <div className="w-full place-content-center p-2 grid grid-cols-[repeat(auto-fill,200px)] grid-rows-[repeat(auto-fill,100px)] gap-2">
+        {chambresses.isLoading && <div>Loading chambresses...</div>}
+        {(chambresses.data ?? []).map((c) => (
+          <ChambressTile key={c.id} chambress={c} />
+        ))}
       </div>
     </div>
   )
@@ -43,15 +44,11 @@ const Home: NextPage = () => {
 
 export default Home
 
-function ChambressTile({ Chambress }: { Chambress: Chambress | null }) {
-  if (congress == null) return null
+function ChambressTile({ chambress }: { chambress: Chambress | null }) {
+  if (chambress == null) return null
   return (
     <div className="rounded-lg bg-neutral-800 w-[200px] h-[100px] p-2">
-      <ul>
-        <li>congress num: {congress?.congress}</li>
-        <li>start year: {congress?.startYear}</li>
-        <li>end year {congress?.endYear}</li>
-      </ul>
+      {chambress.congress} {chambress.chamber}
     </div>
   )
 }
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,8 +3,16 @@ import { createRouter } from './context'
 import superjson from 'superjson'
 import { z } from 'zod'
 import { apiFetch } from '../patterns/fetching'
-import fetch from 'node-fetch'
+import fetch, { Response } from 'node-fetch'
 import { Chambress, Chamber } from '@prisma/client'
+import { flow, identity, pipe, absurd } from 'fp-ts/lib/function'
+import * as E from 'fp-ts/lib/Either'
+import * as TE from 'fp-ts/lib/TaskEither'
+import * as IO from 'fp-ts/lib/IO'
+import * as IE from 'fp-ts/lib/IOEither'
+import * as A from 'fp-ts/lib/Array'
+import * as O from 'fp-ts/lib/Option'
+import { option } from 'fp-ts'
 
 const API_KEY = process.env.CONGRESS_GOV_API_KEY
 const API_BASE_URL = process.env.CONGRESS_GOV_API_BASE_URL
@@ -19,41 +27,47 @@ const PrismaChamberToChamberEnum: Record<Chamber, ChamberEnum> = {
   [Chamber.HOUSE]: 'House of Representatives',
   [Chamber.SENATE]: 'Senate',
 }
+function congressNameToNumber(name: string): number | null {
+  const n = Number.parseInt(name)
+  return Number.isNaN(n) ? null : n
+}
+export function isNotNullOrUndefined<T>(
+  maybe: T | null | undefined,
+): maybe is T {
+  return maybe !== undefined && maybe != null
+}
+export function filterNulls<T>(xs: (T | null)[]): T[] {
+  return xs.filter(isNotNullOrUndefined)
+}
+class CongressAPIError extends Error {
+  public _tag: 'CongressAPIError'
+  public response: Response
+  private constructor(response: Response) {
+    super(
+      `congress API error response [${response.status}]: ${response.statusText}`,
+    )
+    this._tag = 'CongressAPIError'
+    this.response = response
+  }
+  public static of(response: Response): CongressAPIError {
+    return new CongressAPIError(response)
+  }
+}
 
-// Responses as a zod objects and types using z.infer()
 const congressResponseValidator = z.object({
-  endYear: z.string().length(4, { message: 'Must be 4 characters long' }),
-  startYear: z.string().length(4, { message: 'Must be 4 characters long' }),
-  name: z
-    .string()
-    .min(4)
-    .max(5, { message: 'Must be between 4 and 5 characters long' }),
-  number: z
-    .number()
-    .int()
-    // this is probably excessive
-    .min(93)
-    .max(117, {
-      message: `Must be in range 93-${117}`,
-    }),
-  sessions: z.array(
+  congresses: z.array(
     z.object({
-      chamber: ChamberEnum,
-      endDate: z.string(),
-      number: z.number().int(),
-      startDate: z.string(),
+      endYear: z.string(),
+      startYear: z.string(),
+      name: z.string(), // ###th Congress
+      sessions: z.array(z.object({ chamber: ChamberEnum })),
     }),
   ),
 })
 
-const fullResponseValidator = z.array(
-  z.object({
-    congress: congressResponseValidator,
-  }),
-)
-
-type CongressResponse = z.infer<typeof congressResponseValidator>
-type FullResponse = z.infer<typeof fullResponseValidator>
+function errorIdentity(err: unknown): Error {
+  return err instanceof Error ? err : new Error(`${err}`)
+}
 
 export const appRouter = createRouter()
   .transformer(superjson)
@@ -68,20 +82,57 @@ export const appRouter = createRouter()
   })
   .mutation('create-chambresses', {
     async resolve({ ctx }) {
-      const res = await fetch(`${API_BASE_URL}/congress?api_key=${API_KEY}`, {
-        headers: { accept: 'application/json' },
-      })
-      const allCongresses = fullResponseValidator.parse(await res.json())
-      const chambresses = allCongresses
-        .filter((c) => c.congress.number >= 93)
-        .flatMap(({ congress: { number } }) => [
-          { congress: number, chamber: Chamber.HOUSE },
-          { congress: number, chamber: Chamber.SENATE },
-        ])
-      return await ctx.prisma.chambress.createMany({
-        data: chambresses,
-        skipDuplicates: true,
-      })
+      const res = await pipe(
+        TE.tryCatch(
+          () =>
+            fetch(`${API_BASE_URL}?limit=40&api_key=${API_KEY}`, {
+              headers: { accept: 'application/json' },
+            }),
+          errorIdentity,
+        ),
+        TE.chain((res) =>
+          res.status === 200
+            ? TE.right(res)
+            : TE.left(CongressAPIError.of(res)),
+        ),
+        TE.chain((res) => TE.tryCatch(() => res.json(), errorIdentity)),
+        TE.chainEitherK((json) => {
+          const parsed = congressResponseValidator.safeParse(json)
+          return parsed.success ? E.right(parsed.data) : E.left(parsed.error)
+        }),
+        TE.chainEitherK(({ congresses }) =>
+          E.of(
+            pipe(
+              congresses,
+              A.filterMap(({ name }) => {
+                const congress = congressNameToNumber(name)
+                if (congress == null || congress < 93) return option.none
+                return option.some([
+                  { congress, chamber: Chamber.HOUSE },
+                  { congress, chamber: Chamber.SENATE },
+                ])
+              }),
+              A.flatten,
+            ),
+          ),
+        ),
+        TE.chain((chambresses) =>
+          TE.tryCatch(
+            () =>
+              ctx.prisma.chambress.createMany({
+                data: chambresses,
+                skipDuplicates: true,
+              }),
+            errorIdentity,
+          ),
+        ),
+      )()
+
+      if (E.isRight(res)) {
+        return res.right
+      } else {
+        throw res.left
+      }
     },
   })
   .query('get-chambresses', {
