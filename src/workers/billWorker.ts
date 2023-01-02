c16983e7d9281bd65432467396b670932a07ce46
Nick Zeppos <zepposn@gmail.com>
2023-01-02 10:23:21 -0500

    write bill worker logic

--- src/workers/billWorker.ts
+++ src/workers/billWorker.ts
@@ -1,26 +1,63 @@
+import { prisma } from '../server/db/client'
 import { fetchCongressAPI } from './congressAPI'
 import { BillJobData, BillJobName, BillJobResponse } from './types'
+import {
+  billActionsResponseValidator,
+  billResponseValidator,
+} from './validators'
 import { Job } from 'bullmq'
 
 export default async function (
   job: Job<BillJobData, BillJobResponse, BillJobName>,
-) {
+): Promise<BillJobResponse> {
   const t0 = Date.now()
   console.log(`[JOB] started at ${t0}`)
   const { congress, billNum, billType } = job.data
   const route = `/bill/${congress}/${billType}/${billNum}`
   const res = await fetchCongressAPI(route, { format: 'json' })
   const json = await res.json()
-  console.log(json)
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true)
-    }, 5000 + 5000 * Math.random())
+
+  const { bill } = billResponseValidator.parse(json)
+  console.log('bill arrived')
+  const actionsRes = await fetchCongressAPI(`${route}/actions`, {
+    format: json,
   })
-  if (Math.random() < 0.2) {
-    throw new Error('something bad happened')
-  }
+
+  const actionsJson = await actionsRes.json()
+  const { actions } = billActionsResponseValidator.parse(actionsJson)
+
+  const chambressId = await prisma.chambress.findUniqueOrThrow({
+    where: {
+      congress_chamber: {
+        congress: congress,
+        chamber: billType === 'hr' ? 'HOUSE' : 'SENATE',
+      },
+    },
+    select: {
+      id: true,
+    },
+  })
+
+  const sponsorId = await prisma.member.findUniqueOrThrow({
+    where: {
+      bioguideId: bill.sponsors[0].bioguideId,
+    },
+    select: { bioguideId: true },
+  })
+
+  const billRecord = await prisma.bill.create({
+    data: {
+      billNum: billNum,
+      title: bill.title,
+      chambressId: chambressId.id,
+      sponsorId: sponsorId.bioguideId,
+      actions: actions.map((a) => a.text),
+    },
+  })
+
   const t = Date.now() - t0
-  const message = `Pondered bill ${job.data.billNum} in the ${job.data.congress} ${job.data.billType} for ${t}ms`
+  const message = `Received ${congress}-${billType}-${billNum}. Created billRecord ${
+    billRecord.id
+  } in ${t / 1000}s`
   return { message }
 }
