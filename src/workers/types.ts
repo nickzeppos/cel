8c654c00e5cd4303275203b9d16bcbd379a44200
Nick Zeppos <zepposn@gmail.com>
2023-01-02 10:23:09 -0500

    bill job data as validator and inferred type

--- src/workers/types.ts
+++ src/workers/types.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 export interface TestJobData {
   color: string
   count: number
@@ -7,11 +9,19 @@ export interface TestJobResponse {
 }
 export type TestJobName = 'test-job'
 
-export interface BillJobData {
-  congress: number
-  billType: string
-  billNum: number
-}
+// export interface BillJobData {
+//   congress: number
+//   billType: string
+//   billNum: number
+// }
+
+export const billJobDataValidator = z.object({
+  congress: z.number().min(93),
+  billType: z.enum(['hr', 's']),
+  billNum: z.number(),
+})
+
+export type BillJobData = z.infer<typeof billJobDataValidator>
 
 export interface BillJobResponse {
   message: string
