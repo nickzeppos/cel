7f8903b80f8beca9ee3a6eb0a3e00785184d3389
Nick Zeppos <zepposn@gmail.com>
2023-03-31 18:39:58 -0400

    tests for members asset filtering

---
+++ src/assets/utilts.test.ts
@@ -0,0 +1,48 @@
+import { AllMember } from '../workers/validators'
+import { servedIncludes1973 } from './utils'
+
+describe('servedIncludes1973', () => {
+  const served: AllMember['served'] = {
+    House: [
+      {
+        end: 1972,
+        start: 1968,
+      },
+    ],
+  }
+
+  it('should return false if start and end are before 1973', () => {
+    expect(servedIncludes1973(served)).toBe(false)
+  })
+  it('should return true if start is before 1973 but end is not', () => {
+    served.House![0]!.end! = 1974
+    expect(servedIncludes1973(served)).toBe(true)
+  })
+  it('should return true start and end both on or after 1973', () => {
+    served.House![0]!.start! = 1973
+    expect(servedIncludes1973(served)).toBe(true)
+  })
+  it('should behave similarly for Senate', () => {
+    delete served.House
+    served.Senate = [
+      {
+        end: 1972,
+        start: 1968,
+      },
+    ]
+    expect(servedIncludes1973(served)).toBe(false)
+    served.Senate![0]!.end! = 1974
+    expect(servedIncludes1973(served)).toBe(true)
+    served.Senate![0]!.start! = 1973
+    expect(servedIncludes1973(served)).toBe(true)
+  })
+  it('should work if one of the chambers includes 1973 but the other doesnt', () => {
+    served.House = [
+      {
+        end: 1972,
+        start: 1968,
+      },
+    ]
+    expect(servedIncludes1973(served)).toBe(true)
+  })
+})
